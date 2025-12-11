db3d98fe7ad2d4ab4e2133b98af80419513f5531
onuralpArlsn <onuralparslan97@gmail.com>
2025-12-12 01:01:13 +0300

    porti porti

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 9000;
 
 // Serve static files from the current directory
 app.use(express.static(__dirname));
